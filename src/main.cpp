27650153487d466085c74ef47a8f7f7dad647ca1
Ryan Atkins <ratkins@uci.edu>
2017-03-11 23:28:17 -0800

    Correct Row/Column numbering in print_board()

--- src/main.cpp
+++ src/main.cpp
@@ -58,11 +58,11 @@ void initialize_board(string (&b)[ROWS][COLS]){
 
 void print_board(string (&b)[ROWS][COLS]){
 	cout << endl;
-	cout << "     0     1     2     3     4     5     6     7" << endl;
+	cout << "     1     2     3     4     5     6     7     8" << endl;
 	cout << "  |-----|-----|-----|-----|-----|-----|-----|-----|" << endl;
 
 	for(int i = 0; i < ROWS; i++){
-		cout << i << " | ";
+		cout << i+1 << " | ";
 		for(int j = 0; j < COLS; j++){
 			cout << b[i][j] << " | ";
 		}
