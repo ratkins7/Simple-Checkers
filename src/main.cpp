11273d13d6423b9d1a33b783fffabba6a3115a16
Ryan Atkins <ratkins@uci.edu>
2017-03-11 21:58:58 -0800

    Update to board format -> More readable printout

--- src/main.cpp
+++ src/main.cpp
@@ -58,14 +58,16 @@ void initialize_board(string (&b)[ROWS][COLS]){
 
 void print_board(string (&b)[ROWS][COLS]){
 	cout << endl;
-	cout << "   0  1  2  3  4  5  6  7 \n";
+	cout << "     0     1     2     3     4     5     6     7" << endl;
+	cout << "  |-----|-----|-----|-----|-----|-----|-----|-----|" << endl;
 
 	for(int i = 0; i < ROWS; i++){
-		cout << i << " ";
+		cout << i << " | ";
 		for(int j = 0; j < COLS; j++){
-			cout << b[i][j];
+			cout << b[i][j] << " | ";
 		}
 		cout << "\n";
+		cout << "  |-----|-----|-----|-----|-----|-----|-----|-----|" << endl;
 	}
 
 	cout << endl;
